6f4c815007983064372a49ae459bbe954306ba72
BanuRekha1 <57714685+BanuRekha1@users.noreply.github.com>
2020-03-31 16:45:21 +0530

    list View widget

--- app/src/main/java/com/example/widgetsampleapp/NewAppWidget.java
+++ app/src/main/java/com/example/widgetsampleapp/NewAppWidget.java
@@ -5,62 +5,64 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.net.Uri;
 import android.widget.RemoteViews;
-
-import java.text.DateFormat;
-import java.util.Date;
-
-/**
- * Implementation of App Widget functionality.
- */
+import android.widget.Toast;
 public class NewAppWidget extends AppWidgetProvider {
-
-    private static final String mSharedPrefFile =
-            "com.example.android.appwidgetsample";
-    private static final String COUNT_KEY = "count";
-
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
-
-        SharedPreferences prefs = context.getSharedPreferences(
-                mSharedPrefFile, 0);
-        int count = prefs.getInt(COUNT_KEY + appWidgetId, 0);
-        count++;
-        String dateString =
-                DateFormat.getTimeInstance(DateFormat.SHORT).format(new Date());
-
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
-        views.setTextViewText(R.id.appwidget_id, String.valueOf(appWidgetId));
-        views.setTextViewText(R.id.appwidget_update,
-                context.getResources().getString(
-                        R.string.date_count_format, count, dateString));
-
-        SharedPreferences.Editor prefEditor = prefs.edit();
-        prefEditor.putInt(COUNT_KEY + appWidgetId, count);
-        prefEditor.apply();
-
-        Intent intentUpdate = new Intent(context, NewAppWidget.class);
-        intentUpdate.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-        int[] idArray = new int[]{appWidgetId};
-        intentUpdate.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, idArray);
-
-        PendingIntent pendingUpdate = PendingIntent.getBroadcast(
-                context, appWidgetId, intentUpdate,
-                PendingIntent.FLAG_UPDATE_CURRENT);
-
-        views.setOnClickPendingIntent(R.id.button_update, pendingUpdate);
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
+    public static final String TOAST_ACTION = "com.example.android.stackwidget.TOAST_ACTION";
+    public static final String EXTRA_ITEM = "com.example.android.stackwidget.EXTRA_ITEM";
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        super.onDeleted(context, appWidgetIds);
     }
-
+    @Override
+    public void onDisabled(Context context) {
+        super.onDisabled(context);
+    }
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+    }
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        AppWidgetManager mgr = AppWidgetManager.getInstance(context);
+        if (intent.getAction().equals(TOAST_ACTION)) {
+            int appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
+                    AppWidgetManager.INVALID_APPWIDGET_ID);
+            int viewIndex = intent.getIntExtra(EXTRA_ITEM, 0);
+            Toast.makeText(context, "Touched view " + viewIndex, Toast.LENGTH_SHORT).show();
+        }
+        super.onReceive(context, intent);
+    }
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+        // update each of the widgets with the remote adapter
+        for (int i = 0; i < appWidgetIds.length; ++i) {
+            // Here we setup the intent which points to the StackViewService which will
+            // provide the views for this collection.
+            Intent intent = new Intent(context, WidgetService.class);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
+            // When intents are compared, the extras are ignored, so we need to embed the extras
+            // into the data so that the extras will not be ignored.
+            intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+            RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+            rv.setRemoteAdapter(appWidgetIds[i], R.id.list_view, intent);
+            // The empty view is displayed when the collection has no items. It should be a sibling
+            // of the collection view.
+            rv.setEmptyView(R.id.list_view, R.id.empty_view);
+            // Here we setup the a pending intent template. Individuals items of a collection
+            // cannot setup their own pending intents, instead, the collection as a whole can
+            // setup a pending intent template, and the individual items can set a fillInIntent
+            // to create unique before on an item to item basis.
+            Intent toastIntent = new Intent(context, NewAppWidget.class);
+            toastIntent.setAction(NewAppWidget.TOAST_ACTION);
+            toastIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
+            intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+            PendingIntent toastPendingIntent = PendingIntent.getBroadcast(context, 0, toastIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT);
+            rv.setPendingIntentTemplate(R.id.list_view, toastPendingIntent);
+            appWidgetManager.updateAppWidget(appWidgetIds[i], rv);
         }
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
     }
 }
-
